59eb63e9cf55ec702faa984f074adb8ad1e505f5
xApnea <jcbruno150@gmail.com>
2020-07-24 17:55:05 -0400

    Proxy server is working properly

--- proxy-server/server/index.js
+++ proxy-server/server/index.js
@@ -1,3 +1,4 @@
+require('newrelic');
 const express = require('express');
 const path = require('path');
 const { createProxyMiddleware } = require('http-proxy-middleware');
@@ -8,21 +9,21 @@ const rewardsServiceRoute =
   'http://ec2-3-133-92-215.us-east-2.compute.amazonaws.com:3005';
 
 const proxyRouter = {
-  '/api/banner': 'http://localhost:3002',
-  'api/video': 'http://localhost:3002',
-  'api/update': 'http://localhost:3001',
-  'api/comment': 'http://localhost:3001',
-  'api/story': 'http://localhost:3003',
-  'api/RisksAndChallenges': 'http://localhost:3003',
-  'api/EnvironmentalCommitments': 'http://localhost:3003',
-  'api/projects': rewardsServiceRoute,
-  'api/rewards': rewardsServiceRoute,
+  // '/api/banner': 'http://localhost:3002',
+  // 'api/video': 'http://localhost:3002',
+  'api/update': 'ec2-3-15-166-80.us-east-2.compute.amazonaws.com:3001',
+  'api/comment': 'ec2-3-15-166-80.us-east-2.compute.amazonaws.com:3001',
+  // 'api/story': 'http://localhost:3003',
+  // 'api/RisksAndChallenges': 'http://localhost:3003',
+  // 'api/EnvironmentalCommitments': 'http://localhost:3003',
+  // 'api/projects': rewardsServiceRoute,
+  // 'api/rewards': rewardsServiceRoute,
 };
 
 app.use(express.static(path.join(__dirname, '../dist')));
 
 app.get('/:id', (req, res) => {
-  res.sendFile(path.join(__dirname, '../dist', 'index.html'));
+  res.sendFile(path.join(__dirname, '../public', 'index.html'));
 });
 
 app.use(
